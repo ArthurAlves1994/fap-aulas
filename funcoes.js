b2e78e1034d29876832d53e6feb2e195423a6c7e
ArthurAlves1994 <141767510+ArthurAlves1994@users.noreply.github.com>
2023-08-08 19:58:50 +0000

    aula fucoes

--- funcoes.js
+++ funcoes.js
@@ -1,1 +1,14 @@
-console.log("arthur")
+// function imprimirLista(nome, idade, dia, mes, ano) {
+//     console.log(`Boa tarde ${ nome }, notei que vc tem ${ idade }, hoje e ${ dia }, de ${ mes }, de ${ ano }`)
+// }
+// imprimirLista("arthur",29,8,"agosto",2023)
+
+// function saudacao(saudacao){console.log(saudacao);}
+// saudacao("ola mundo")
+
+// function dobro(num) {console.log(num*2);}
+// dobro(10)
+
+// function mostrarNumero(inicio, fim){console.log(`informe os numeros para inicio e ${inicio}, fim ${fim}`);}
+// mostrarNumero(10,20)
+
